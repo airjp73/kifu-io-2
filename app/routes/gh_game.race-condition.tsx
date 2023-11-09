ca48560ab549db05c5b1407fcee286ecb026e8bc
Aaron Pettengill <pettengill.aaron@gmail.com>
2023-11-09 17:10:02 -0500

    actually commit the race condition page

---
+++ app/routes/gh_game.race-condition.tsx
@@ -0,0 +1,11 @@
+export default function Fail() {
+  return (
+    <div>
+      <h2>Whoops!</h2>
+      <p>
+        Looks like someone else played a move at the same time as you and theirs
+        went through first.
+      </p>
+    </div>
+  );
+}
